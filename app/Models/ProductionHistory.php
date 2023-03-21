637dab5008b8a73a371408ebd35b70e7372ea678
Kaleemullah <Kaleemullahdev@gmail.com>
2023-03-21 11:19:12 +0500

    show raw material and product name in production

--- app/Models/ProductionHistory.php
+++ app/Models/ProductionHistory.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Http\Traits\FilterByUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductionHistory extends Model
 {
@@ -17,4 +18,15 @@ class ProductionHistory extends Model
         'owner_id',
         'product_id'
     ];
+
+    public function RawMaterial():BelongsTo
+    {
+        return $this->belongsTo('App\Models\Purchase','purchase_id','id');
+    }
+
+
+    public function Product():BelongsTo
+    {
+        return $this->belongsTo('App\Models\Product','product_id','id');
+    }
 }
