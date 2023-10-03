25a0687564eb034113ae252ccdbc69f0dd5916b2
Kaleemullah <Kaleemullahdev@gmail.com>
2023-10-03 19:59:58 +0500

    Added Route for Deposit History

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\DepositHistoryController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
@@ -78,6 +79,7 @@ Route::group([
         Route::post('get-customers',[CustomerController::class,'getCustomers']); // Pending
         Route::resource('customer',CustomerController::class);
         Route::resource('vendor',VendorController::class);
+        Route::resource('deposit',DepositHistoryController::class);
 
 
 });
